f052269d52ed86ce70898476c3e71ca149bd20c4
Souleymane DIALLO <diallosouleymane.07@gmail.com>
2017-04-04 15:49:29 +0200

    ayayouy

--- conduct.c
+++ conduct.c
@@ -17,91 +17,100 @@ struct conduct{
 }conduct;
 
 struct conduct *conduct_create(const char *name, size_t a, size_t c){
-struct conduct* cond= (struct conduct*) malloc(sizeof(conduct));
-cond->a=(size_t*)malloc(sizeof(size_t));
-cond->c=(size_t*)malloc(sizeof(size_t));
-*cond->a=a;
-*cond->c=c;
-if(name==NULL){
-cond->addr=(int*)mmap(NULL, c, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-  if(cond->addr==MAP_FAILED){
-    perror("conduct_create failed");
-    exit(-1);
+  struct conduct* cond= (struct conduct*) malloc(sizeof(conduct));
+  cond->a=(size_t*)malloc(sizeof(size_t));
+  cond->c=(size_t*)malloc(sizeof(size_t));
+  *cond->a=a;
+  *cond->c=c;
+  if(name==NULL){
+    cond->addr=(int*)mmap(NULL, c, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if(cond->addr==MAP_FAILED){
+      perror("conduct_create failed");
+      exit(-1);
+    }
   }
-}
-else{
-  cond->name=strdup(name);
-  int fd=open(name, O_CREAT | O_RDWR, 0666);
-  if(fd==NULL){
-    perror("no file descriptor error open");
-    exit(-1);
-  }
-  cond->addr=(int*)mmap(NULL, c, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-  if(cond->addr == MAP_FAILED){
-    perror("erreur map failed");
-    exit(-1);
-  }
-  char b[1000];
-  if(sprintf(b, "%p",cond)<0){
-    perror("failed to print");
-    exit(-1);
-  }
-  if(write(fd, b, strlen(b))==-1){
-    perror("failed to write");
-    exit(-1);
-  }
+  else{
+    cond->name=strdup(name);
+    int fd=open(name, O_CREAT | O_RDWR, 0666);
+    if(fd == -1){
+      perror("no file descriptor error open");
+      exit(-1);
+    }
+    cond->addr=(int*)mmap(NULL, c, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if(cond->addr == MAP_FAILED){
+      perror("erreur map failed");
+      exit(-1);
+    }
+    char b[1000];
+    if(sprintf(b, "%p",cond)<0){
+      perror("failed to print");
+      exit(-1);
+    }
+    if(write(fd, b, strlen(b))==-1){
+      perror("failed to write");
+      exit(-1);
+    }
 
-  if(close(fd)==-1){
-    perror("failed to close file");
-    exit(-1);
+    if(close(fd)==-1){
+      perror("failed to close file");
+      exit(-1);
+    }
   }
-}
-return cond;
+  return cond;
 }
 
 struct conduct *conduct_open(const char*name){
-int fd=open(name, O_RDONLY);
-if(fd==-1){
-  perror("failed to open the file");
-  exit(-1);
-}
-struct conduct *cond=(struct conduct*)malloc(sizeof(struct conduct));
-char *b=(char *)malloc(1000*sizeof(char));
-if(read(fd, b, strlen(b))==-1){
-  perror("failed to read the file");
-  exit(-1);
-}
-if(close(fd)==-1){
-  perror("failed to close the file");
-  exit(-1);
-}
-cond=(struct conduct*)b;
-return cond;
+  int fd=open(name, O_RDONLY);
+  if(fd == -1){
+    perror("failed to open the file");
+    exit(-1);
+  }
+  struct conduct *cond=(struct conduct*)malloc(sizeof(struct conduct));
+  char *b=(char *)malloc(1000*sizeof(char));
+  if(read(fd, b, strlen(b))==-1){
+    perror("failed to read the file");
+    exit(-1);
+  }
+  if(close(fd)==-1){
+    perror("failed to close the file");
+    exit(-1);
+  }
+  cond = (struct conduct*)b;
+  return cond;
 }
 
 ssize_t conduct_read(struct conduct *c, void *buf, size_t count){
 
+  return 1;
 }
 
 ssize_t conduct_write(struct conduct *c, const void *buf, size_t count){
 
+  return 1;
 }
 
 int conduct_write_eof(struct conduct *c){
 
+  return 1;
 }
 
 void conduct_close(struct conduct *conduct){
-free(conduct);
+  free(conduct);
 }
 
 void conduct_destroy(struct conduct *conduct){
-munmap(conduct->addr, *conduct->c);
-free(conduct->a);
-free(conduct->c);
-if(conduct->name == NULL){
-  unlink(conduct->name);
-  free(conduct->name);
-}
-conduct_close(conduct);
+  if(munmap(conduct->addr, *conduct->c) == -1){
+    perror("failed while unmaping");
+    exit(-1);
+  }
+  free(conduct->a);
+  free(conduct->c);
+  if(conduct->name == NULL){
+    if (unlink(conduct->name) == -1) {
+      perror("unlink failed");
+      exit(-1);
+    }
+    free(conduct->name);
+  }
+  conduct_close(conduct);
 }
