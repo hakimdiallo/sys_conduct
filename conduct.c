a9f5e01169c6eb56f4f37e703252410b75ee72d5
Souleymane DIALLO <diallosouleymane.07@gmail.com>
2017-04-25 14:08:30 +0200

    new buffer

--- conduct.c
+++ conduct.c
@@ -12,21 +12,21 @@
 
 struct conduct{
   char *name;
-  size_t *c;
-  size_t *a;
+  size_t c;
+  size_t a;
+  size_t eof;
+  size_t current_size;
+  pthread_mutex_t verrou;
+  pthread_cond_t empty_conduct;
+  pthread_cond_t full_conduct;
+  pthread_cond_t not_enough_place;
   void *buffer;
-  size_t *eof;
-  size_t *current_size;
-  pthread_mutex_t *verrou;
-  pthread_cond_t *empty_conduct;
-  pthread_cond_t *full_conduct;
-  pthread_cond_t *not_enough_place;
 }conduct;
 
 struct conduct *conduct_create(const char *name, size_t a, size_t c){
   struct conduct* cond;
   if(name==NULL){
-    cond = (struct conduct*) mmap(NULL, sizeof(struct conduct *), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    cond = mmap(NULL, sizeof(struct conduct)+c, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     if(cond == MAP_FAILED){
       perror("conduct_create failed");
       exit(-1);
@@ -47,28 +47,19 @@ struct conduct *conduct_create(const char *name, size_t a, size_t c){
 
 
   }
-  memset(cond, 0, (sizeof(struct conduct)));
+  memset(cond, 0, (sizeof(struct conduct)+c));
 
-  cond->verrou = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
-  cond->empty_conduct = (pthread_cond_t *)malloc(sizeof(pthread_cond_t));
-  cond->full_conduct = (pthread_cond_t *)malloc(sizeof(pthread_cond_t));
-  cond->not_enough_place = (pthread_cond_t *)malloc(sizeof(pthread_cond_t));
-  cond->buffer=(void*)malloc(sizeof(void)*c);
-  cond->a = (size_t*) malloc(sizeof(size_t));
-  cond->c = (size_t*) malloc(sizeof(size_t));
-  cond->eof = (size_t*) malloc(sizeof(size_t));
-
-
-  pthread_mutex_init(cond->verrou, NULL);
-  pthread_cond_init(cond->empty_conduct,NULL);
-  pthread_cond_init(cond->full_conduct,NULL);
-  pthread_cond_init(cond->not_enough_place,NULL);
-  *cond->a=a;
-  *cond->c=c;
-  *cond->eof = 0;
-  cond->current_size = (size_t *)malloc(sizeof(size_t));
-  *cond->current_size = 0;
-
+  pthread_mutex_init(&cond->verrou, NULL);
+  pthread_cond_init(&cond->empty_conduct,NULL);
+  pthread_cond_init(&cond->full_conduct,NULL);
+  pthread_cond_init(&cond->not_enough_place,NULL);
+  cond->a=a;
+  cond->c=c;
+  cond->eof = 0;
+  cond->buffer = (void *) cond + sizeof(struct conduct);
+  //cond->current_size = (size_t *)malloc(sizeof(size_t));
+  cond->current_size = 0;
+  //memset(cond+sizeof(struct conduct)+c, 0, cond->c);
   return cond;
 }
 
@@ -88,91 +79,93 @@ struct conduct *conduct_open(const char*name){
 }
 
 ssize_t conduct_read(struct conduct *c, void *buf, size_t count){
-  //printf("Thread lecture veut prendre le verrou %zd\n", *c->a);
-  pthread_mutex_lock(c->verrou);
+  printf("Thread lecture veut prendre le verrou %zd\n", c->a);
+  pthread_mutex_lock(&c->verrou);
   //printf("Thread lecture prend le verrou\n");
-    while (*c->current_size == 0) {
-      if(*c->eof == 0){
+    while (c->current_size == 0) {
+      if(c->eof == 0){
     //    printf("Thread lecture pas de eof s'endort sur vide\n");
-        pthread_cond_wait(c->empty_conduct, c->verrou);
+        pthread_cond_wait(&c->empty_conduct, &c->verrou);
     //  printf("Thread lecture se reveille\n");
       }else{
         return 0;
       }
     }
-    size_t m = minimum(*c->current_size, count);
+    size_t m = minimum(c->current_size, count);
     memcpy(buf,c->buffer,m);
-    if (m == *c->current_size) {
-      memset(c->buffer, 0, *c->c);
+    if (m == c->current_size) {
+      memset(c->buffer, 0, c->c);
     }
     else{
-      void *tmp = (void *)malloc(*c->c*sizeof(void));
-      memset(tmp, 0, *c->c);
-      memcpy(tmp, c->buffer+m, *c->current_size - m);
-      memset(c->buffer, 0, *c->c);
-      memcpy(c->buffer, tmp, *c->current_size - m);
+      /*void *tmp = 0;// = (void *)malloc(*c->c*sizeof(void));
+      memset(tmp, 0, c->c);
+      memcpy(tmp, c+(sizeof(struct conduct *))+m, c->current_size - m);
+      memset(c+(sizeof(struct conduct *)), 0, c->c);
+      memcpy(c+(sizeof(struct conduct *)), tmp, c->current_size - m);*/
+      memmove(c->buffer, c->buffer+m, c->current_size - m);
+      memset(c->buffer+(c->current_size -m ), 0, c->c - (c->current_size - m));
     }
 //    printf("%d\n", (int)*c->current_size);
-    *c->current_size = *c->current_size - m;
+    c->current_size = c->current_size - m;
 //  printf("%d\n", (int)*c->current_size);
 //    printf("Thread lecture broadcast sur full\n");
-    pthread_cond_broadcast(c->not_enough_place);
-    pthread_cond_broadcast(c->full_conduct);
-  pthread_mutex_unlock(c->verrou);
-//  printf("Thread lecture relache le verrou\n");
+    pthread_cond_broadcast(&c->not_enough_place);
+    pthread_cond_broadcast(&c->full_conduct);
+  pthread_mutex_unlock(&c->verrou);
+  printf("Thread lecture relache le verrou\n");
   return m;
 }
 
 ssize_t conduct_write(struct conduct *c, const void *buf, size_t count){
-//  printf("Thread ecriture veut prendre le verrou\n");
-  pthread_mutex_lock(c->verrou);
+  printf("Thread ecriture veut prendre le verrou\n");
+  pthread_mutex_lock(&c->verrou);
 //  printf("Thread ecriture prend le verrou\n");
   while (1) {
-    if (*c->current_size == *c->c) {
-  //    printf("Thread ecriture s'endort sur full\n");
-      //pthread_cond_broadcast(c->empty_conduct);
-      pthread_cond_wait(c->full_conduct, c->verrou);
-    //  printf("Thread ecriture se reveille de full\n");
-    }
-    if (*c->eof == 1) {
+    if (c->eof == 1) {
       // errno
       //printf("Thread ecriture il ya eof\n");
-      pthread_mutex_unlock(c->verrou);
+      pthread_mutex_unlock(&c->verrou);
       //printf("Thread ecriture relache le verrou\n");
       return -1;
     }
-    if ((count <= *c->a && count <= (*c->c-*c->current_size)) || (count > *c->a && count <= (*c->c-*c->current_size))) {
-      memcpy(c->buffer+(*c->current_size),buf,count);
-      *c->current_size = *c->current_size  + count;
+    if (c->current_size == c->c) {
+  //    printf("Thread ecriture s'endort sur full\n");
+      //pthread_cond_broadcast(c->empty_conduct);
+      pthread_cond_wait(&c->full_conduct, &c->verrou);
+    //  printf("Thread ecriture se reveille de full\n");
+    }
+    if ((count <= c->a && count <= (c->c-c->current_size)) || (count > c->a && count <= (c->c-c->current_size))) {
+      memcpy(c->buffer+c->current_size,buf,count);
+      c->current_size = c->current_size  + count;
       //printf("Thread ecriture broadcast sur empty\n");
-      pthread_cond_broadcast(c->empty_conduct);
-      pthread_mutex_unlock(c->verrou);
-      //printf("Thread ecriture relache le verrou\n");
+      pthread_cond_broadcast(&c->empty_conduct);
+      pthread_mutex_unlock(&c->verrou);
+      printf("Thread ecriture relache le verrou\n");
       return count;
     }
-    else if (count > *c->a && count > (*c->c-*c->current_size)) {
-      size_t m = *c->c - *c->current_size;
-      memcpy(c->buffer+*c->current_size,buf,m);
-      *c->current_size = *c->current_size  + m;
+    else if (count > c->a && count > (c->c-c->current_size)) {
+      size_t m = c->c - c->current_size;
+      memcpy(c->buffer+c->current_size,buf,m);
+      c->current_size = c->current_size  + m;
       //printf("Thread ecriture broadcast sur empty\n");
-      pthread_cond_broadcast(c->empty_conduct);
-      pthread_mutex_unlock(c->verrou);
-      //printf("Thread ecriture relache le verrou\n");
+      pthread_cond_broadcast(&c->empty_conduct);
+      pthread_mutex_unlock(&c->verrou);
+      printf("Thread ecriture relache le verrou\n");
       return m;
     }
     else {
       //printf("Thread ecriture s'endort sur not_enough_place\n");
       //pthread_cond_broadcast(c->empty_conduct);
-      pthread_cond_wait(c->not_enough_place, c->verrou);
+      pthread_cond_wait(&c->full_conduct, &c->verrou);
       //printf("Thread ecriture se reveille de not_enough_place\n");
     }
   }
 }
 
 int conduct_write_eof(struct conduct *c){
-  if (*c->eof == 0) {
-    *c->eof = 1;
-    pthread_cond_broadcast(c->empty_conduct);
+  if (c->eof == 0) {
+    c->eof = 1;
+    pthread_cond_broadcast(&c->empty_conduct);
   }
   return 0;
 }
@@ -182,12 +175,12 @@ void conduct_close(struct conduct *conduct){
 }
 
 void conduct_destroy(struct conduct *conduct){
-  if(munmap(conduct->buffer, *conduct->c) == -1){
+  if(munmap(conduct->buffer, conduct->c) == -1){
     perror("failed while unmaping");
     exit(-1);
   }
-  free(conduct->a);
-  free(conduct->c);
+  //free(conduct->a);
+  //free(conduct->c);
   if(conduct->name == NULL){
     if (unlink(conduct->name) == -1) {
       perror("unlink failed");
